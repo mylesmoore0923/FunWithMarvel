2a4d2b71c6a5badedcecf74bddf5845202fc3fd3
myles <student@admins-MacBook-Pro-41.local>
2019-04-20 15:22:19 -0700

    challenge 1 working on 2 & 3

--- script.js
+++ script.js
@@ -2,20 +2,47 @@ $(document).ready(function(){
 
 
 //All code here
+let movieList = [
+  "Captain America: The First Avenger",
+  "Captain Marvel",
+  "Iron Man",
+  "The Incredible Hulk",
+  "Iron Man 2",
+  "Thor",
+  "The Avengers",
+  "Iron Man 3",
+  "Thor: The Dark World",
+  "Captain America: The Winter Soldier",
+  "Guardians of the Galaxy",
+  "Guardians of the Galaxy Vol. 2",
+  "Avengers: Age of Ultron",
+  "Ant-Man",
+  "Captain America: Civil War",
+  "Doctor Strange",
+  "Black Panther",
+  "Spider-Man-Homecoming",
+  "Thor: Ragnarok",
+  "Ant-Man and the Wasp",
+  "Avengers: Infinity War",
+]
+for  (let i = 0; i < movieList.length; i++){
+  let newElement = `<h1>${movieList[i]}</h1>`
+  console.log(newElement)
 
+$("#display-area").append(newElement)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+let movieIronMan = {
+    timelinePosition: 3,
+    title: "Iron Man",
+    moviePoster: "images/poster-ironman.jpg",
+    releaseDate: "May 2, 2008",
+    releasePosition: 1,
+    description: "2008's Iron Man tells the story of Tony Stark, a billionaire industrialist and genius inventor who is kidnapped and forced to build a devastating weapon. Instead, using his intelligence and ingenuity, Tony builds a high-tech suit of armor and escapes captivity. When he uncovers a nefarious plot with global implications, he dons his powerful armor and vows to protect the world as Iron Man.",
+    budget: "140,000,000",
+    gross: "585,174,222",
+    rottenTomatoes: "93%"
+  }
 
 
 
